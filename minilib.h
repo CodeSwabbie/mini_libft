6be423fbbc2ce42c8c475137adaef5a5fb254c76
Wiktor Kabat <kabatu@srak-book.home>
2024-06-29 17:14:49 +0200

    Minilib created

---
+++ minilib.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilibft.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkabat <wkabat@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 14:50:29 by wkabat            #+#    #+#             */
+/*   Updated: 2024/06/29 14:57:01 by wkabat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINILIB_H
+# define MINILIB_H
+
+# include "gnl/get_next_line.h"
+# include "printf/ft_printf.h"
+# include "libft/libft.h"
+
+#endif
